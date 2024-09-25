8daafad4e54238a4e843632cbaa56f3f1c33f41f
dunno.wut2do <92363678+Ouyin66@users.noreply.github.com>
2024-09-25 13:01:42 +0700

    new

---
+++ lib/login.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({super.key});
+
+  @override
+  State<LoginWidget> createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 300),
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(28),
+                  topRight: Radius.circular(28),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(25),
+                child: Center(
+                  child: Column(),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
